1fa4ea0d6f2b4fcf3a79e7d7d802861545b1f5fe
Diego Emmel <diegoemmel0@gmail.com>
2021-01-27 19:19:26 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/diegoemmel/cursomc/CursomcApplication.java
+++ src/main/java/com/diegoemmel/cursomc/CursomcApplication.java
@@ -103,7 +103,7 @@ public class CursomcApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "00000000000", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "00000000009", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("000000001", "000000002"));
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "00000001", cli1, c1);
